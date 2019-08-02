21ed4ea09d7ba5ff7f800d827fe892c9661423bf
Jonas Russer <jonas.russer@gmail.com>
2019-08-02 20:54:26 +0200

    add print_sudoku()

---
+++ readme.md
@@ -0,0 +1,12 @@
+Automatische Sudokuloesung finden durch rekursive Programmierung
+
+Version 1 (current master):
+Zeige das Board in der Konsole
+
+Version 2 (noch keine Branch erstellt): 
+Nutze tkinter fuer GUI Anzeige
+
+
+1. Finde leeres Feld (Feld == 0)
+2. Finde erste valide Zahl zwischen 1-9 und schreibe diese in Feld (x,y)
+3. Falls keine valide Zahl gefunden schreibe Feld auf 0 und suche neue valide Zahl fuer Feld davor
--- sudoku.py
+++ sudoku.py
@@ -9,3 +9,18 @@ board = [
     [0,0,0,4,1,9,0,0,5],
     [0,0,0,0,0,0,0,7,0]
 ]
+
+def print_sudoku(board):
+    for i in range(len(board[0])):
+        if i % 3 == 0 and i != 0:            
+            print("- - - - - - - -")
+        for j in range(len(board[i])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+            if j != 8:
+                print(board[i][j],end="")
+            else:
+                print(board[i][j])
+            
+
+print_sudoku(board)
